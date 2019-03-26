bdf56db1119b02803bbf21d5bbff8d1c9252f2b1
Jarl-Erik Malmström <jarl-erik.malmstrom@wunderdog.fi>
2019-03-26 09:40:34 +0200

    Some minor change.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World, from Nodejs with Kubernetes!\n');
+    res.send('Hello World, from Nodejs with Kubernetes with new version!\n');
 });
 
 app.listen(PORT, HOST);
