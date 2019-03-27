b7e1f8fda92ea9533821cbba6efc50d018701749
Jarl-Erik Malmström <jarl-erik.malmstrom@wunderdog.fi>
2019-03-27 11:17:44 +0200

    Print hard coded version.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World, from Nodejs with Kubernetes with new version!\n');
+    res.send('Hello World, from Nodejs with Kubernetes with version: master_1.0.13?\n');
 });
 
 app.listen(PORT, HOST);
